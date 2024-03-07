687433ed835609dd9a7fc975d4eda5a25fcf2f17
Sanju2op <120462833+Sanju2op@users.noreply.github.com>
2024-03-07 18:08:59 +0530

    login modification

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -11,15 +11,15 @@ const LogIn = () => {
     <div className="container-fluid">
       {step === 1 ?(
         <>
-          <h1 className="alert alert-danger">Remember Your Email and then click Continue</h1>
-          <button className="btn btn-success" onClick={() => setStep(2)}>
+          <h1 className="alert alert-primary rounded-5">Remember Your Email and then click Continue</h1>
+          <button className="btn btn-success rounded-4" onClick={() => setStep(2)}>
             Continue with email
           </button>
         </>
       ) : (
         <>
-          <h1 className="alert alert-danger">Remember Your Password and then click Log in</h1>
-          <button className="btn btn-success" onClick={() => setStep(2)}>
+          <h1 className="alert alert-primary rounded-5">Remember Your Password and then click Log in</h1>
+          <button className="btn btn-success rounded-4" onClick={() => setStep(1)}>
             Log in
           </button>
         </>
